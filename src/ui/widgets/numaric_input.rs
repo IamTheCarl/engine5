9889fe787da827608a915dcc1353d3833508d81f
James Carl <jamescarl96@gmail.com>
2023-08-23 14:07:34 -0400

    Add copy/paste support to numeric inputs.

--- src/ui/widgets/numaric_input.rs
+++ src/ui/widgets/numaric_input.rs
@@ -7,10 +7,11 @@ use bevy_ui_navigation::{
     prelude::{Focusable, Focused, MenuBuilder, MenuSetting, NavEvent, NavRequest},
     NavRequestSystem,
 };
+use std::fmt::Write;
 
-use crate::ui::OverlayMenu;
+use crate::ui::{Clipboard, OverlayMenu};
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Sign {
     Posative,
     Negative,
@@ -31,7 +32,7 @@ impl std::fmt::Display for Sign {
     }
 }
 
-#[derive(Component)]
+#[derive(Debug, Component, Eq, PartialEq)]
 pub struct NumaricInput {
     /// The sign of the value, can be positive or negative.
     /// Is ignored when converting to unsigned integers.
@@ -40,6 +41,103 @@ pub struct NumaricInput {
     // Starts with least significant value, ends with most significant value.
     value_integer: Vec<u8>,
     value_fractional: Vec<u8>,
+}
+
+impl ToString for NumaricInput {
+    fn to_string(&self) -> String {
+        let mut value = String::new();
+
+        if Sign::Negative == self.sign {
+            value += "-";
+        }
+
+        let mut integer_iterator = self.value_integer.iter().peekable();
+
+        while integer_iterator.peek() == Some(&&0u8) {
+            integer_iterator.next();
+        }
+
+        if integer_iterator.peek().is_none() {
+            value = "0".into();
+        } else {
+            for digit in integer_iterator {
+                write!(&mut value, "{:01}", digit).ok();
+            }
+        }
+
+        if !self.value_fractional.is_empty() {
+            write!(&mut value, ".").ok();
+
+            for digit in self.value_fractional.iter() {
+                write!(&mut value, "{:01}", digit).ok();
+            }
+        }
+
+        value
+    }
+}
+
+#[test]
+fn string_formatting() {
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0],
+            value_fractional: vec![]
+        }
+        .to_string(),
+        "0"
+    );
+
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0, 0],
+            value_fractional: vec![]
+        }
+        .to_string(),
+        "0"
+    );
+
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0],
+            value_fractional: vec![0, 0]
+        }
+        .to_string(),
+        "0.00"
+    );
+
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Negative,
+            value_integer: vec![0],
+            value_fractional: vec![]
+        }
+        .to_string(),
+        "0"
+    );
+
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Negative,
+            value_integer: vec![1],
+            value_fractional: vec![]
+        }
+        .to_string(),
+        "-1"
+    );
+
+    assert_eq!(
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0, 0, 1],
+            value_fractional: vec![]
+        }
+        .to_string(),
+        "1"
+    );
 }
 
 impl NumaricInput {
@@ -107,6 +205,33 @@ impl NumaricInput {
 
         value
     }
+
+    fn update_from_parts(&mut self, sign: Sign, integer: &str, fractional: &str) {
+        self.sign = sign;
+
+        // Clear the number out first.
+        self.value_integer.iter_mut().for_each(|value| *value = 0);
+
+        self.value_integer
+            .iter_mut()
+            .rev()
+            .zip(integer.chars().rev())
+            .for_each(|(target, source)| {
+                *target = source.to_digit(10).unwrap() as u8;
+            });
+
+        // Do it again for the fractional part.
+        self.value_fractional
+            .iter_mut()
+            .for_each(|value| *value = 0);
+
+        self.value_fractional
+            .iter_mut()
+            .zip(fractional.chars())
+            .for_each(|(target, source)| {
+                *target = source.to_digit(10).unwrap() as u8;
+            });
+    }
 }
 
 #[test]
@@ -128,6 +253,55 @@ fn numaric_input() {
     assert_eq!(value.as_signed::<i64>(), -123);
 
     assert!((value.as_f64() - -123.123) < 0.001);
+}
+
+#[test]
+fn update_from_parts() {
+    let mut value = NumaricInput {
+        sign: Sign::Posative,
+        value_integer: vec![0, 0],
+        value_fractional: vec![0, 0],
+    };
+
+    value.update_from_parts(Sign::Posative, "1", "2");
+    assert_eq!(
+        value,
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0, 1],
+            value_fractional: vec![2, 0],
+        }
+    );
+
+    value.update_from_parts(Sign::Posative, "123", "4");
+    assert_eq!(
+        value,
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![2, 3],
+            value_fractional: vec![4, 0],
+        }
+    );
+
+    value.update_from_parts(Sign::Posative, "1", "234");
+    assert_eq!(
+        value,
+        NumaricInput {
+            sign: Sign::Posative,
+            value_integer: vec![0, 1],
+            value_fractional: vec![2, 3],
+        }
+    );
+
+    value.update_from_parts(Sign::Negative, "1", "2");
+    assert_eq!(
+        value,
+        NumaricInput {
+            sign: Sign::Negative,
+            value_integer: vec![0, 1],
+            value_fractional: vec![2, 0],
+        }
+    );
 }
 
 /// Spawn a numric input.
@@ -418,6 +592,64 @@ fn update_digit_text(
     }
 }
 
+fn get_numaric_input<'a, R>(
+    integer_digits: impl Iterator<Item = &'a DigitReference>,
+    fractional_digits: impl Iterator<Item = &'a DigitReference>,
+    signs: impl Iterator<Item = &'a SignReference>,
+    numaric_entities: &Query<&NumaricInput>,
+
+    func: impl FnOnce(&NumaricInput) -> R,
+) -> Option<R> {
+    for integer_digit in integer_digits {
+        if let Ok(numaric_input) = numaric_entities.get(integer_digit.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    for fraction_digit in fractional_digits {
+        if let Ok(numaric_input) = numaric_entities.get(fraction_digit.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    for sign in signs {
+        if let Ok(numaric_input) = numaric_entities.get(sign.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    None
+}
+
+fn get_numaric_input_mut<'a, R>(
+    integer_digits: impl Iterator<Item = &'a DigitReference>,
+    fractional_digits: impl Iterator<Item = &'a DigitReference>,
+    signs: impl Iterator<Item = &'a SignReference>,
+    numaric_entities: &mut Query<&mut NumaricInput>,
+
+    func: impl FnOnce(Mut<NumaricInput>) -> R,
+) -> Option<R> {
+    for integer_digit in integer_digits {
+        if let Ok(numaric_input) = numaric_entities.get_mut(integer_digit.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    for fraction_digit in fractional_digits {
+        if let Ok(numaric_input) = numaric_entities.get_mut(fraction_digit.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    for sign in signs {
+        if let Ok(numaric_input) = numaric_entities.get_mut(sign.numaric_input_entity) {
+            return Some(func(numaric_input));
+        }
+    }
+
+    None
+}
+
 fn get_digit_mut<'a>(
     integer_digits: impl Iterator<Item = &'a DigitReference>,
     fractional_digits: impl Iterator<Item = &'a DigitReference>,
@@ -631,7 +863,134 @@ fn process_digit_keyboard_input(
     }
 }
 
+fn parse_number(input: &str) -> nom::IResult<&str, (Sign, &str, &str)> {
+    use nom::{
+        branch::alt,
+        character::complete::{char as nom_char, digit0, multispace0},
+        combinator::{success, value},
+        sequence::{delimited, pair, separated_pair, terminated},
+        Parser,
+    };
+
+    let (input, (sign, (integer, fractional))) = delimited(
+        multispace0,
+        pair(
+            terminated(
+                alt((
+                    value(Sign::Posative, nom_char('+')),
+                    value(Sign::Negative, nom_char('-')),
+                ))
+                .or(success(Sign::Posative)),
+                multispace0,
+            ),
+            separated_pair(
+                digit0,
+                delimited(multispace0, nom_char('.'), multispace0),
+                digit0,
+            ),
+        ),
+        multispace0,
+    )(input)?;
+
+    Ok((input, (sign, integer, fractional)))
+}
+
+#[test]
+fn number_parsing() {
+    assert_eq!(
+        parse_number("+123.456"),
+        Ok(("", (Sign::Posative, "123", "456")))
+    );
+    assert_eq!(
+        parse_number("+ 123 . 456 "),
+        Ok(("", (Sign::Posative, "123", "456")))
+    );
+    assert_eq!(
+        parse_number("123.456"),
+        Ok(("", (Sign::Posative, "123", "456")))
+    );
+    assert_eq!(
+        parse_number("-123.456"),
+        Ok(("", (Sign::Negative, "123", "456")))
+    );
+}
+
 // TODO Support copy and pasting of numbers.
+#[allow(clippy::complexity)]
+fn copy(
+    keyboard_status: Res<Input<KeyCode>>,
+
+    numaric_entities: Query<&NumaricInput>,
+    integer_digits: Query<
+        &DigitReference,
+        (With<Focused>, With<IntegerDigit>, Without<FractionalDigit>),
+    >,
+    fractional_digits: Query<
+        &DigitReference,
+        (With<Focused>, With<FractionalDigit>, Without<IntegerDigit>),
+    >,
+    signs: Query<&SignReference, With<Focused>>,
+    mut clipboard: ResMut<Clipboard>,
+) {
+    if keyboard_status.pressed(KeyCode::ControlLeft) && keyboard_status.just_pressed(KeyCode::C) {
+        get_numaric_input(
+            integer_digits.iter(),
+            fractional_digits.iter(),
+            signs.iter(),
+            &numaric_entities,
+            |numaric_input| {
+                match clipboard.set_contents(numaric_input.to_string()) {
+                    Ok(()) => {
+                        // Linux requires this, for some reason...
+                        // https://github.com/alacritty/copypasta/issues/49
+                        clipboard.get_contents().ok();
+                    }
+                    Err(error) => {
+                        log::error!("Failed to copy number to system clipboard: {:?}", error);
+                    }
+                }
+            },
+        );
+    }
+}
+
+#[allow(clippy::complexity)]
+fn paste(
+    keyboard_status: Res<Input<KeyCode>>,
+
+    mut numaric_entities: Query<&mut NumaricInput>,
+    integer_digits: Query<
+        &DigitReference,
+        (With<Focused>, With<IntegerDigit>, Without<FractionalDigit>),
+    >,
+    fractional_digits: Query<
+        &DigitReference,
+        (With<Focused>, With<FractionalDigit>, Without<IntegerDigit>),
+    >,
+    signs: Query<&SignReference, With<Focused>>,
+    mut clipboard: ResMut<Clipboard>,
+) {
+    if keyboard_status.pressed(KeyCode::ControlLeft) && keyboard_status.just_pressed(KeyCode::V) {
+        get_numaric_input_mut(
+            integer_digits.iter(),
+            fractional_digits.iter(),
+            signs.iter(),
+            &mut numaric_entities,
+            |mut numaric_input| match clipboard.get_contents() {
+                Ok(content) => {
+                    if let Ok((_leftovers, (sign, integer, fractional))) = parse_number(&content) {
+                        numaric_input.update_from_parts(sign, integer, fractional);
+                    } else {
+                        log::error!("Failed to parse number from system clipboard.");
+                    }
+                }
+                Err(error) => {
+                    log::error!("Failed to paste number from system clipboard: {:?}", error);
+                }
+            },
+        );
+    }
+}
 
 pub fn setup(app: &mut App) {
     app.add_systems(
@@ -643,6 +1002,8 @@ pub fn setup(app: &mut App) {
             process_digit_general_input.after(NavRequestSystem),
             process_digit_mouse_input,
             process_digit_keyboard_input,
+            copy,
+            paste,
         ),
     );
 }

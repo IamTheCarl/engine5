9b5a2a1c209c19ce00bf9772df66cd117ee686f4
James Carl <jamescarl96@gmail.com>
2023-09-03 02:10:11 -0400

    Add network connection commands.

--- src/multiplayer/mod.rs
+++ src/multiplayer/mod.rs
@@ -1,18 +1,236 @@
+use std::{
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket},
+    time::{Duration, SystemTime},
+};
+
+use anyhow::{anyhow, Context, Result};
 use bevy::prelude::*;
+use renet::{
+    transport::{
+        ClientAuthentication, NetcodeClientTransport, NetcodeDisconnectReason,
+        NetcodeServerTransport, ServerAuthentication, ServerConfig,
+    },
+    ConnectionConfig, RenetClient, RenetServer,
+};
+
+use crate::GameState;
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
 pub struct MultiplayerPlugin;
 
 impl Plugin for MultiplayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<MultiplayerState>();
+        // If we unload the world, we disconnect from the host or end our hosting session.
+        app.add_systems(OnExit(GameState::InGame), |mut commands: Commands| {
+            commands.remove_resource::<HostContext>();
+            commands.remove_resource::<ClientContext>();
+        });
+
+        HostContext::setup(app);
+        ClientContext::setup(app);
     }
 }
 
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
-pub enum MultiplayerState {
-    #[default]
-    Offline,
-    Server,
-    Client,
+#[derive(Resource)]
+pub struct HostContext {
+    server: RenetServer,
+    transport: NetcodeServerTransport,
+}
+
+impl HostContext {
+    fn setup(app: &mut App) {
+        app.add_systems(
+            Update,
+            (Self::update_host.run_if(resource_exists::<Self>()),),
+        );
+    }
+
+    pub fn start_session(
+        commands: &mut Commands,
+        socket_address: SocketAddr,
+        max_clients: usize,
+    ) -> Result<()> {
+        let socket = UdpSocket::bind(socket_address).context("Failed to bind UDP socket.")?;
+        let server_config = ServerConfig {
+            max_clients,
+            protocol_id: 0,
+            public_addr: socket_address,
+            authentication: ServerAuthentication::Unsecure,
+        };
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .context("Failed to get current time.")?;
+
+        let transport = NetcodeServerTransport::new(current_time, server_config, socket)
+            .context("Failed to build transport layer.")?;
+
+        let server = RenetServer::new(ConnectionConfig::default());
+
+        commands.insert_resource(Self { server, transport });
+
+        Ok(())
+    }
+
+    pub fn end_session(commands: &mut Commands) {
+        commands.remove_resource::<Self>();
+    }
+
+    fn update(&mut self, time_delta: Duration) -> Result<()> {
+        self.server.update(time_delta);
+        self.transport
+            .update(time_delta, &mut self.server)
+            .context("Error in transport layer.")?;
+
+        // Announce client connections.
+        while let Some(event) = self.server.get_event() {
+            match event {
+                renet::ServerEvent::ClientConnected { client_id } => {
+                    log::info!("New client: {}", client_id);
+                }
+                renet::ServerEvent::ClientDisconnected { client_id, reason } => {
+                    log::info!("Client {} disconnected. Reason: {}", client_id, reason);
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn update_host(mut context: ResMut<Self>, mut commands: Commands, time: Res<Time>) {
+        if let Err(error) = context.update(time.delta()) {
+            log::error!("Error in transport layer: {:?}", error);
+            log::warn!("Hosting session will be terminated.");
+
+            commands.remove_resource::<Self>();
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct ClientContext {
+    client_id: u64,
+    client: RenetClient,
+    transport: NetcodeClientTransport,
+    addresses: Box<dyn Iterator<Item = SocketAddr> + Sync + Send>,
+}
+
+impl ClientContext {
+    fn setup(app: &mut App) {
+        app.add_systems(
+            Update,
+            (Self::update_client.run_if(resource_exists::<Self>()),),
+        );
+    }
+
+    pub fn start_session(
+        commands: &mut Commands,
+        mut addresses: Box<dyn Iterator<Item = SocketAddr> + Sync + Send>,
+        client_id: u64,
+    ) -> Result<()> {
+        let remote_address = addresses
+            .next()
+            .context("No addresses available for server.")?;
+
+        let (client, transport) = Self::create_client_transport_pair(remote_address, client_id)?;
+
+        commands.insert_resource(Self {
+            client_id,
+            client,
+            transport,
+            addresses,
+        });
+
+        Ok(())
+    }
+
+    fn select_local_socket(remote_address: &SocketAddr) -> Result<SocketAddr> {
+        let port = port_selector::random_free_udp_port()
+            .context("Could not find any open ports on the local system to connect with.")?;
+
+        // In many operating systems, you can only connect to an IPV6 from an IPV6
+        // and only an IPV4 can communicate with an IPV4, so make sure to select the right kind of socket when doing that.
+        let local_ip_address = if remote_address.is_ipv4() {
+            IpAddr::V4(Ipv4Addr::UNSPECIFIED)
+        } else {
+            IpAddr::V6(Ipv6Addr::UNSPECIFIED)
+        };
+
+        Ok(SocketAddr::new(local_ip_address, port))
+    }
+
+    fn create_client_transport_pair(
+        remote_address: SocketAddr,
+        client_id: u64,
+    ) -> Result<(RenetClient, NetcodeClientTransport)> {
+        log::info!("Trying address `{}`", remote_address);
+
+        let socket = UdpSocket::bind(Self::select_local_socket(&remote_address)?)
+            .context("Failed to bind UDP socket.")?;
+        let client_config = ClientAuthentication::Unsecure {
+            protocol_id: 0,
+            client_id,
+            server_addr: remote_address,
+            user_data: None,
+        };
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .context("Failed to get current time.")?;
+
+        let transport = NetcodeClientTransport::new(current_time, client_config, socket)
+            .context("Failed to build transport layer.")?;
+
+        let client = RenetClient::new(ConnectionConfig::default());
+
+        Ok((client, transport))
+    }
+
+    fn try_new_address(&mut self) -> Result<()> {
+        let remote_address = self
+            .addresses
+            .next()
+            .context("No more addresses available for server.")?;
+
+        let (client, transport) =
+            Self::create_client_transport_pair(remote_address, self.client_id)?;
+
+        self.transport = transport;
+        self.client = client;
+
+        Ok(())
+    }
+
+    pub fn end_session(commands: &mut Commands) {
+        commands.remove_resource::<Self>();
+    }
+
+    fn update(&mut self, time_delta: Duration) -> Result<()> {
+        self.client.update(time_delta);
+        match self.transport.update(time_delta, &mut self.client) {
+            Err(error) => {
+                match self.transport.disconnect_reason() {
+                    Some(NetcodeDisconnectReason::ConnectionRequestTimedOut) => {
+                        // Nobody replied.
+                        // Try a different address.
+                        self.try_new_address()
+                    }
+                    _ => Err(anyhow!(dbg!(error))),
+                }
+            }
+            _ => {
+                // TODO collect messages and distribute them.
+
+                Ok(())
+            }
+        }
+    }
+
+    fn update_client(mut context: ResMut<Self>, mut commands: Commands, time: Res<Time>) {
+        if let Err(error) = context.update(time.delta()) {
+            log::error!("Error in transport layer: {:?}", error);
+            log::warn!("Client session will be terminated.");
+
+            commands.remove_resource::<Self>();
+        }
+    }
 }
--- src/world/commands.rs
+++ src/world/commands.rs
@@ -1,11 +1,19 @@
-use std::path::Path;
+use std::{
+    net::{IpAddr, SocketAddr},
+    path::Path,
+};
 
 use bevy::prelude::*;
 
+use anyhow::{Context, Result};
 use bevy_console::{reply, AddConsoleCommand, ConsoleCommand};
 use clap::{Parser, Subcommand};
 
-use crate::{config::file_paths, GameState};
+use crate::{
+    config::file_paths,
+    multiplayer::{ClientContext, HostContext},
+    GameState,
+};
 
 use super::{terrain::BlockRegistry, WorldState};
 
@@ -45,19 +53,29 @@ enum WorldSubcommand {
     /// Connect to an already running multi-player game.
     Connect {
         /// The host of the game. Can be a raw IP address or hostname.
-        host: String,
+        ip_address: String,
 
         /// The port number of the host.
-        #[arg(default_value = "5000")]
+        #[arg(default_value = "5000", short, long)]
         port: u16,
+
+        #[arg(default_value = "0", short, long)]
+        client_id: u64,
     },
 
     /// Host the current world as a multi-player game.
     /// The world must already be loaded.
     Host {
         /// The port number to host on.
-        #[arg(default_value = "5000")]
+        #[arg(default_value = "5000", short, long)]
         port: u16,
+
+        #[arg(default_value = "0.0.0.0", short, long)]
+        ip_address: IpAddr,
+
+        /// The maximum number of clients we permit to join the session.
+        #[arg(default_value = "20", short, long)]
+        max_players: usize,
     },
 
     /// Close connection to remote server or end session you are currently hosting.
@@ -82,99 +100,201 @@ fn list_worlds(log: &mut ConsoleCommand<WorldCommand>) {
 
 fn world_command(
     mut log: ConsoleCommand<WorldCommand>,
-    mut commands: Commands,
+    commands: Commands,
 
     game_state: Res<State<GameState>>,
-    mut next_app_state: ResMut<NextState<GameState>>,
-    mut next_world_state: ResMut<NextState<WorldState>>,
+    next_app_state: ResMut<NextState<GameState>>,
+    next_world_state: ResMut<NextState<WorldState>>,
 
     block_registry: Res<BlockRegistry>,
 ) {
     if let Some(Ok(WorldCommand { command })) = log.take() {
         match game_state.get() {
-            GameState::MainMenu | GameState::FatalError => match command {
-                WorldSubcommand::Load { name } => {
-                    if let Err(error) = super::open_world(
-                        &mut commands,
-                        &block_registry,
-                        Path::new(file_paths::SAVE_DIRECTORY).join(name),
-                    ) {
-                        reply!(log, "Failed to load world: {:?}", error);
-                    } else {
-                        reply!(log, "World successfully loaded.");
-                        next_app_state.set(GameState::InGame);
-                        next_world_state.set(WorldState::Running);
-                    }
-                }
-                WorldSubcommand::Unload => {
-                    reply!(log, "World is already unloaded.");
-                }
-                WorldSubcommand::Create { name } => {
-                    if let Err(error) = super::create_world(
-                        &mut commands,
-                        &block_registry,
-                        Path::new(file_paths::SAVE_DIRECTORY).join(name),
-                    ) {
-                        reply!(log, "Failed to create world: {:?}", error);
-                    } else {
-                        reply!(log, "World successfully created.");
-                        next_app_state.set(GameState::InGame);
-                        next_world_state.set(WorldState::Running);
-                    }
-                }
-                WorldSubcommand::Delete { name } => {
-                    let path = Path::new(file_paths::SAVE_DIRECTORY).join(name);
-
-                    if path.exists() {
-                        if let Err(error) = std::fs::remove_dir_all(path) {
-                            reply!(log, "Failed to delete world: {:?}", error);
-                        }
-                    } else {
-                        reply!(log, "World does not exist.");
-                    }
-                }
-                WorldSubcommand::List => list_worlds(&mut log),
-                WorldSubcommand::Connect { host, port } => reply!(log, "Unimplemented."),
-                WorldSubcommand::Host { port: _ } => {
-                    reply!(log, "You must load a world before you can host it.");
-                }
-                WorldSubcommand::Close => reply!(log, "No world is being hosted."),
-            },
+            GameState::MainMenu | GameState::FatalError => world_command_in_menu(
+                command,
+                log,
+                commands,
+                next_app_state,
+                next_world_state,
+                block_registry,
+            ),
             GameState::InGame => {
-                match command {
-                    WorldSubcommand::Load { name: _ } => {
-                        reply!(
-                            log,
-                            "You must unload the world before you can load another one."
-                        );
-                    }
-                    WorldSubcommand::Unload => {
-                        next_app_state.set(GameState::MainMenu);
-                        next_world_state.set(WorldState::Unloaded);
-                    }
-                    WorldSubcommand::Create { name: _ } => {
-                        reply!(
-                            log,
-                            "You must unload the world before you can create a new one."
-                        );
-                    }
-                    WorldSubcommand::Delete { name: _ } => {
-                        reply!(
-                            log,
-                            "You must unload the world before you can delete a world."
-                        )
-                    }
-                    WorldSubcommand::List => list_worlds(&mut log),
-                    WorldSubcommand::Connect { host: _, port: _ } => {
-                        reply!(log, "You muat unload the current world before you can connect to another one.")
-                    }
-                    WorldSubcommand::Host { port } => reply!(log, "Unimplemented."),
-                    WorldSubcommand::Close => reply!(log, "Unimplemented."),
-                }
+                world_command_in_game(command, log, commands, next_app_state, next_world_state)
             }
             GameState::ShuttingDown => {
                 reply!(log, "Cannot manipulate worlds while shutting down.");
             }
+        }
+    }
+}
+
+fn world_command_in_menu(
+    command: WorldSubcommand,
+    mut log: ConsoleCommand<WorldCommand>,
+
+    mut commands: Commands,
+
+    mut next_app_state: ResMut<NextState<GameState>>,
+    mut next_world_state: ResMut<NextState<WorldState>>,
+
+    block_registry: Res<BlockRegistry>,
+) {
+    match command {
+        WorldSubcommand::Load { name } => {
+            if let Err(error) = super::open_world(
+                &mut commands,
+                &block_registry,
+                Path::new(file_paths::SAVE_DIRECTORY).join(name),
+            ) {
+                reply!(log, "Failed to load world: {:?}", error);
+            } else {
+                reply!(log, "World successfully loaded.");
+                next_app_state.set(GameState::InGame);
+                next_world_state.set(WorldState::Running);
+            }
+        }
+        WorldSubcommand::Unload => {
+            reply!(log, "World is already unloaded.");
+        }
+        WorldSubcommand::Create { name } => {
+            if let Err(error) = super::create_world(
+                &mut commands,
+                &block_registry,
+                Path::new(file_paths::SAVE_DIRECTORY).join(name),
+            ) {
+                reply!(log, "Failed to create world: {:?}", error);
+            } else {
+                reply!(log, "World successfully created.");
+                next_app_state.set(GameState::InGame);
+                next_world_state.set(WorldState::Running);
+            }
+        }
+        WorldSubcommand::Delete { name } => {
+            let path = Path::new(file_paths::SAVE_DIRECTORY).join(name);
+
+            if path.exists() {
+                if let Err(error) = std::fs::remove_dir_all(path) {
+                    reply!(log, "Failed to delete world: {:?}", error);
+                }
+            } else {
+                reply!(log, "World does not exist.");
+            }
+        }
+        WorldSubcommand::List => list_worlds(&mut log),
+        WorldSubcommand::Connect {
+            ip_address,
+            port,
+            client_id,
+        } => {
+            if let Err(error) = world_connect(&mut commands, ip_address, port, client_id) {
+                reply!(log, "Failed to connect to host: {:?}", error);
+            }
+        }
+        WorldSubcommand::Host {
+            port: _,
+            ip_address: _,
+            max_players: _,
+        } => {
+            reply!(log, "You must load a world before you can host it.");
+        }
+        WorldSubcommand::Close => reply!(log, "No world is being hosted."),
+    }
+}
+
+fn world_connect(
+    commands: &mut Commands,
+    ip_address: String,
+    port: u16,
+    client_id: u64,
+) -> Result<()> {
+    use std::cmp::Ordering;
+
+    let mut addresses = dns_lookup::lookup_host(&ip_address).context("DNS lookup failed.")?;
+
+    // We do this sort to make IPv4 take higher priority.
+    // It'll result in players connecting faster most of the time.
+    // TODO: Should this be an option in the settings?
+    addresses.sort_by(|a, b| {
+        if a.is_ipv4() && b.is_ipv6() {
+            Ordering::Less
+        } else if a.is_ipv6() && b.is_ipv4() {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    });
+
+    let addresses = addresses
+        .into_iter()
+        .map(move |ip_addr| SocketAddr::new(ip_addr, port));
+    ClientContext::start_session(commands, Box::new(addresses), client_id)
+}
+
+fn world_command_in_game(
+    command: WorldSubcommand,
+    mut log: ConsoleCommand<WorldCommand>,
+
+    mut commands: Commands,
+
+    mut next_app_state: ResMut<NextState<GameState>>,
+    mut next_world_state: ResMut<NextState<WorldState>>,
+) {
+    match command {
+        WorldSubcommand::Load { name: _ } => {
+            reply!(
+                log,
+                "You must unload the world before you can load another one."
+            );
+        }
+        WorldSubcommand::Unload => {
+            next_app_state.set(GameState::MainMenu);
+            next_world_state.set(WorldState::Unloaded);
+        }
+        WorldSubcommand::Create { name: _ } => {
+            reply!(
+                log,
+                "You must unload the world before you can create a new one."
+            );
+        }
+        WorldSubcommand::Delete { name: _ } => {
+            reply!(
+                log,
+                "You must unload the world before you can delete a world."
+            )
+        }
+        WorldSubcommand::List => list_worlds(&mut log),
+        WorldSubcommand::Connect {
+            ip_address: _,
+            port: _,
+            client_id: _,
+        } => {
+            reply!(
+                log,
+                "You must unload the current world before you can connect to another one."
+            )
+        }
+        WorldSubcommand::Host {
+            port,
+            ip_address,
+            max_players,
+        } => {
+            let socket_address = SocketAddr::new(ip_address, port);
+            if let Err(error) =
+                HostContext::start_session(&mut commands, socket_address, max_players)
+            {
+                reply!(log, "Failed to host world: {:?}", error);
+            } else {
+                reply!(
+                    log,
+                    "Hosting world at {} with a max of {} players.",
+                    socket_address,
+                    max_players
+                );
+            }
+        }
+        WorldSubcommand::Close => {
+            HostContext::end_session(&mut commands);
+            reply!(log, "Session ended.");
         }
     }
 }

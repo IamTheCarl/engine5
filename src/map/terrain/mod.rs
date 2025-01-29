bdc03b969fc0126c042ac3e58b16a327c5003832
James Carl <jamescarl96@gmail.com>
2025-01-29 00:08:22 -0500

    AABBs can now list chunks they overlap

--- src/map/terrain/mod.rs
+++ src/map/terrain/mod.rs
@@ -1,24 +1,22 @@
-use std::{
-    collections::HashMap,
-    num::{NonZeroU16, NonZeroU64},
-};
+use std::{num::NonZeroU16, ops::RangeInclusive};
 
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashSet};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 use super::storage::PersistantId;
 
 type VoxelID = NonZeroU16;
-pub type VoxelCoordinate = IVec3;
+pub type VoxelIndex = IVec3;
 type ChunkIndex = IVec3;
-type LocalVoxelCoordinate = UVec3;
+type LocalVoxelIndex = UVec3;
 
 const CHUNK_BIT_SHIFT: u32 = 4;
 const CHUNK_DIAMETER: i32 = 1 << CHUNK_BIT_SHIFT as i32;
 const NUM_BLOCKS: usize =
     CHUNK_DIAMETER as usize * CHUNK_DIAMETER as usize * CHUNK_DIAMETER as usize;
 
-fn calculate_block_indexes(coordinate: VoxelCoordinate) -> (ChunkIndex, LocalVoxelCoordinate) {
+fn calculate_block_indexes(coordinate: VoxelIndex) -> (ChunkIndex, LocalVoxelIndex) {
     let chunk_index: ChunkIndex = coordinate >> CHUNK_BIT_SHIFT;
     let local_block_coordinate = coordinate.as_uvec3() & (!0u32 >> (32u32 - CHUNK_BIT_SHIFT));
 
@@ -34,7 +32,6 @@ impl Plugin for TerrainPlugin {
         app.register_type::<VoxelStorage>();
         app.register_type::<TerrainAABB>();
         app.register_type::<Structure>();
-        app.register_type::<StructureStorage>();
     }
 }
 
@@ -54,48 +51,41 @@ pub struct VoxelStorage {
     Component, Default, Clone, Debug, PartialEq, Eq, Hash, Reflect, Serialize, Deserialize,
 )]
 pub struct TerrainAABB {
-    pub a: VoxelCoordinate,
-    pub b: VoxelCoordinate,
+    pub a: VoxelIndex,
+    pub b: VoxelIndex,
 }
 
 impl TerrainAABB {
-    const KEY_LENGTH: usize = 4 * 6;
-    pub fn storage_key(&self) -> [u8; Self::KEY_LENGTH] {
-        let mut key = [0; Self::KEY_LENGTH];
+    pub fn intersected_chunks(&self) -> impl Iterator<Item = ChunkIndex> {
+        let (chunk_index_a, _local_voxel_coordinate) = calculate_block_indexes(self.a);
+        let (chunk_index_b, _local_voxel_coordinate) = calculate_block_indexes(self.b);
 
-        let ax = self.a.x.to_le_bytes();
-        let ay = self.a.y.to_le_bytes();
-        let az = self.a.z.to_le_bytes();
-        let bx = self.b.x.to_le_bytes();
-        let by = self.b.y.to_le_bytes();
-        let bz = self.b.z.to_le_bytes();
+        fn axis_range<T: Ord + Copy>(a: T, b: T) -> RangeInclusive<T> {
+            let min = a.min(b);
+            let max = a.max(b);
 
-        key[0..=3].copy_from_slice(&ax);
-        key[4..=7].copy_from_slice(&ay);
-        key[8..=11].copy_from_slice(&az);
-        key[12..=15].copy_from_slice(&bx);
-        key[16..=19].copy_from_slice(&by);
-        key[20..=23].copy_from_slice(&bz);
+            min..=max
+        }
 
-        key
+        let x_range = axis_range(chunk_index_a.x, chunk_index_b.x);
+        let y_range = axis_range(chunk_index_a.y, chunk_index_b.y);
+        let z_range = axis_range(chunk_index_a.z, chunk_index_b.z);
+
+        x_range
+            .cartesian_product(y_range)
+            .cartesian_product(z_range)
+            .map(|((x, y), z)| ChunkIndex::new(x, y, z))
     }
-
-    // pub fn intersected_chunks(&self) -> impl Iterator<Item = ChunkIndex> {
-    //     let (chunk_index_a, _local_voxel_coordinate) = calculate_block_indexes(self.a);
-    //     let (chunk_index_b, _local_voxel_coordinate) = calculate_block_indexes(self.b);
-
-    //     todo!()
-    // }
 }
 
 #[derive(Component, Reflect)]
 #[require(TerrainAABB)]
 pub struct Structure;
 
-#[derive(Component, Reflect)]
+#[derive(Component, Serialize, Deserialize)]
 pub struct StructureStorage {
     /// Stores references to multi-block structures.
-    structures: HashMap<TerrainAABB, PersistantId>,
+    structures: HashSet<PersistantId>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -121,49 +111,140 @@ mod test {
     use super::*;
 
     #[test]
-    fn terrain_aabb_key() {
-        let aabb = TerrainAABB {
-            a: VoxelCoordinate::new(0x00_00_00_00, 0x11_11_11_11, 0x22_22_22_22),
-            b: VoxelCoordinate::new(0x33_33_33_33, 0x44_44_44_44, 0x55_55_55_55),
-        };
+    fn aabb_chunk_coverage() {
+        assert_eq!(
+            TerrainAABB {
+                a: VoxelIndex::new(0, 0, 0),
+                b: VoxelIndex::new(0, 0, 0)
+            }
+            .intersected_chunks()
+            .collect::<HashSet<_>>(),
+            HashSet::from([ChunkIndex::new(0, 0, 0)])
+        );
 
         assert_eq!(
-            aabb.storage_key(),
-            [
-                0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22, 0x33, 0x33,
-                0x33, 0x33, 0x44, 0x44, 0x44, 0x44, 0x55, 0x55, 0x55, 0x55
-            ]
+            TerrainAABB {
+                a: VoxelIndex::new(0, 0, 0),
+                b: VoxelIndex::new(CHUNK_DIAMETER, CHUNK_DIAMETER, CHUNK_DIAMETER)
+            }
+            .intersected_chunks()
+            .collect::<HashSet<_>>(),
+            HashSet::from([
+                ChunkIndex::new(0, 0, 0),
+                ChunkIndex::new(0, 0, 1),
+                ChunkIndex::new(0, 1, 0),
+                ChunkIndex::new(0, 1, 1),
+                ChunkIndex::new(1, 0, 0),
+                ChunkIndex::new(1, 0, 1),
+                ChunkIndex::new(1, 1, 0),
+                ChunkIndex::new(1, 1, 1)
+            ])
+        );
+
+        assert_eq!(
+            TerrainAABB {
+                a: VoxelIndex::new(CHUNK_DIAMETER, CHUNK_DIAMETER, CHUNK_DIAMETER),
+                b: VoxelIndex::new(0, 0, 0)
+            }
+            .intersected_chunks()
+            .collect::<HashSet<_>>(),
+            HashSet::from([
+                ChunkIndex::new(0, 0, 0),
+                ChunkIndex::new(0, 0, 1),
+                ChunkIndex::new(0, 1, 0),
+                ChunkIndex::new(0, 1, 1),
+                ChunkIndex::new(1, 0, 0),
+                ChunkIndex::new(1, 0, 1),
+                ChunkIndex::new(1, 1, 0),
+                ChunkIndex::new(1, 1, 1)
+            ])
+        );
+
+        assert_eq!(
+            TerrainAABB {
+                a: VoxelIndex::new(0, 0, 0),
+                b: VoxelIndex::new(-CHUNK_DIAMETER, -CHUNK_DIAMETER, -CHUNK_DIAMETER)
+            }
+            .intersected_chunks()
+            .collect::<HashSet<_>>(),
+            HashSet::from([
+                ChunkIndex::new(0, 0, 0),
+                ChunkIndex::new(0, 0, -1),
+                ChunkIndex::new(0, -1, 0),
+                ChunkIndex::new(0, -1, -1),
+                ChunkIndex::new(-1, 0, 0),
+                ChunkIndex::new(-1, 0, -1),
+                ChunkIndex::new(-1, -1, 0),
+                ChunkIndex::new(-1, -1, -1)
+            ])
+        );
+        assert_eq!(
+            TerrainAABB {
+                a: VoxelIndex::new(CHUNK_DIAMETER, CHUNK_DIAMETER, CHUNK_DIAMETER),
+                b: VoxelIndex::new(-CHUNK_DIAMETER, -CHUNK_DIAMETER, -CHUNK_DIAMETER)
+            }
+            .intersected_chunks()
+            .collect::<HashSet<_>>(),
+            HashSet::from([
+                ChunkIndex::new(-1, -1, -1),
+                ChunkIndex::new(-1, -1, 0),
+                ChunkIndex::new(-1, -1, 1),
+                ChunkIndex::new(-1, 0, -1),
+                ChunkIndex::new(-1, 0, 0),
+                ChunkIndex::new(-1, 0, 1),
+                ChunkIndex::new(-1, 1, -1),
+                ChunkIndex::new(-1, 1, 0),
+                ChunkIndex::new(-1, 1, 1),
+                ChunkIndex::new(0, -1, -1),
+                ChunkIndex::new(0, -1, 0),
+                ChunkIndex::new(0, -1, 1),
+                ChunkIndex::new(0, 0, -1),
+                ChunkIndex::new(0, 0, 0),
+                ChunkIndex::new(0, 0, 1),
+                ChunkIndex::new(0, 1, -1),
+                ChunkIndex::new(0, 1, 0),
+                ChunkIndex::new(0, 1, 1),
+                ChunkIndex::new(1, -1, -1),
+                ChunkIndex::new(1, -1, 0),
+                ChunkIndex::new(1, -1, 1),
+                ChunkIndex::new(1, 0, -1),
+                ChunkIndex::new(1, 0, 0),
+                ChunkIndex::new(1, 0, 1),
+                ChunkIndex::new(1, 1, -1),
+                ChunkIndex::new(1, 1, 0),
+                ChunkIndex::new(1, 1, 1),
+            ])
         );
     }
 
     #[test]
     fn block_index_calculation() {
         let (chunk_index, local_block_coordinate) =
-            calculate_block_indexes(VoxelCoordinate::new(0, 0, 0));
+            calculate_block_indexes(VoxelIndex::new(0, 0, 0));
         assert_eq!(chunk_index, ChunkIndex::new(0, 0, 0));
-        assert_eq!(local_block_coordinate, LocalVoxelCoordinate::new(0, 0, 0));
+        assert_eq!(local_block_coordinate, LocalVoxelIndex::new(0, 0, 0));
 
         let (chunk_index, local_block_coordinate) =
-            calculate_block_indexes(VoxelCoordinate::new(CHUNK_DIAMETER, 0, 0));
+            calculate_block_indexes(VoxelIndex::new(CHUNK_DIAMETER, 0, 0));
         assert_eq!(chunk_index, ChunkIndex::new(1, 0, 0));
-        assert_eq!(local_block_coordinate, LocalVoxelCoordinate::new(0, 0, 0));
+        assert_eq!(local_block_coordinate, LocalVoxelIndex::new(0, 0, 0));
 
         let (chunk_index, local_block_coordinate) =
-            calculate_block_indexes(VoxelCoordinate::new(-CHUNK_DIAMETER, 0, 0));
+            calculate_block_indexes(VoxelIndex::new(-CHUNK_DIAMETER, 0, 0));
         assert_eq!(chunk_index, ChunkIndex::new(-1, 0, 0));
-        assert_eq!(local_block_coordinate, LocalVoxelCoordinate::new(0, 0, 0));
+        assert_eq!(local_block_coordinate, LocalVoxelIndex::new(0, 0, 0));
 
         let (chunk_index, local_block_coordinate) =
-            calculate_block_indexes(VoxelCoordinate::new(-CHUNK_DIAMETER + 1, 0, 0));
+            calculate_block_indexes(VoxelIndex::new(-CHUNK_DIAMETER + 1, 0, 0));
         assert_eq!(chunk_index, ChunkIndex::new(-1, 0, 0));
-        assert_eq!(local_block_coordinate, LocalVoxelCoordinate::new(1, 0, 0));
+        assert_eq!(local_block_coordinate, LocalVoxelIndex::new(1, 0, 0));
 
         let (chunk_index, local_block_coordinate) =
-            calculate_block_indexes(VoxelCoordinate::new(-CHUNK_DIAMETER - 1, 0, 0));
+            calculate_block_indexes(VoxelIndex::new(-CHUNK_DIAMETER - 1, 0, 0));
         assert_eq!(chunk_index, ChunkIndex::new(-2, 0, 0));
         assert_eq!(
             local_block_coordinate,
-            LocalVoxelCoordinate::new(CHUNK_DIAMETER as u32 - 1, 0, 0)
+            LocalVoxelIndex::new(CHUNK_DIAMETER as u32 - 1, 0, 0)
         );
     }
 }
